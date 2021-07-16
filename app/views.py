dd7636d81ef4bda96f5c0e70706b930313ca4d07
Alireza Amouzadeh <alireza@amouzadeh.net>
2021-07-16 11:05:15 +0430

    WIP Mongodb

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from werkzeug.exceptions import abort
+
 from . import app
 from . import models
 
@@ -14,8 +16,17 @@ def create_food():
         description='ماهی با سبزیجات و ترشی انبه'
     )
     food_instance.region = ['بندرعباس', 'هرمز']
+    food_instance.image = open('/home/alireza/Projects/MaktabFlask/app/mahi.jpeg', 'rb')
     food_instance.save()
     return food_instance.to_json(), 201
+
+
+@app.route('/food')
+def get_food():
+    food_instance = models.Food.objects(name='پیتزا').first()
+    if food_instance is None:
+        abort(404)
+    return food_instance.to_json()
 
 
 @app.route('/all')
